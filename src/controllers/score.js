0f64ebfdcb5ab2762dc995e450ec7375904324ca
goddoro <dongho1222@unist.ac.kr>
2020-12-27 16:33:59 +0900

    test

--- src/controllers/score.js
+++ src/controllers/score.js
@@ -18,8 +18,8 @@ export default class scoreController {
                 history : Joi.object().required(),
                 foreign : Joi.object().optional(),
                 line : Joi.string().required(),
-                naesinScore : Joi.number().optional(),
-                naesinType : Joi.string().optional()
+                naesinScore : Joi.optional(),
+                naesinType : Joi.optional()
             })
             
             const { korean , math , english , tamgu1, tamgu2 , history ,foreign, line, naesinScore, naesinType } = result 
