3da05da1730b23f7892b7d78d674b5f4ef820f7c
goddoro <dongho1222@unist.ac.kr>
2021-01-05 01:55:55 +0900

    test

--- dist/controllers/report.js
+++ dist/controllers/report.js
@@ -1015,6 +1015,8 @@ class reportController {
           }
         }
       } else if (extraSubject == "수가") {
+        console.log("2");
+
         if (math_type == "가") {
           if (extraPoint == "수가 백분위 20% 총점에 가산") {
             extraScore.math = score.math.percentile * 0.2;
